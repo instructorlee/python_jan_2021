9c57415d7fc7ae1ec8cc9f6d763f142b2ce398c5
kleelof <kleelof@yahoo.com>
2021-02-03 08:54:16 -0800

    update

--- book_website/book_website/book_manager/urls.py
+++ book_website/book_website/book_manager/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
 
-from .views import index_view
+from .views import index_view, add_book_view, book_view, view_book
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('book/add/', add_book_view, name='add_book'),
+    path('book', book_view, name='book'),
+    path('book/view/<int:book_id>', view_book, name='book_view'),
 ]
+
+# add_book_view
+'''
+    urlpatterns = [
+        path('', index_view, name='index'),
+        path('book/add/', add_book_view, name='add_book_view'), # trailing '/' is required for POST
+        path('book', book_view, name='book')
+    ]
+'''
+
+# 4
+'''
+    path('book/view/<int:book_id>', book_view, name='book_view')
+'''
--- book_website/book_website/book_manager/views.py
+++ book_website/book_website/book_manager/views.py
@@ -1,20 +1,63 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+# 2
 HOURS_OF_OPERATION = [
+    {'day': 'Sunday', 'open': 'closed', 'close': 'closed'},
     {'day': 'Monday', 'open': '8am', 'close': '5pm'},
     {'day': 'Tuesday', 'open': '8am', 'close': '5pm'},
     {'day': 'Wednesday', 'open': 'closed', 'close': 'closed'},
     {'day': 'Thursday', 'open': '8am', 'close': '5pm'},
     {'day': 'Friday', 'open': '8am', 'close': '5pm'},
-    {'day': 'Saturday', 'open': '8am', 'close': '5pm'},
-    {'day': 'Sunday', 'open': 'closed', 'close': 'closed'}
+    {'day': 'Saturday', 'open': '8am', 'close': '5pm'}
 ]
 
-# Create your views here.
-def index_view(request):
 
-    day_of_week = datetime.today().weekday() # returns a number 0 - 7; 0 = Monday
+def add_book_view(request):
+
+    if request.method == 'POST':
+        check_books_list(request)
+
+        # simple data validation
+        title = request.POST.get('title', None)  # request.POST['title']
+        author = request.POST.get('author', None)
+
+        if not title or not author:
+            return redirect(request, '/add_book')
+
+        # save the book
+        request.session['books'].append({
+            'id': len(request.session['books']),
+            'title': title,
+            'author': author
+        })
+        # request.session.save()
+
+        # print(request.session['books'])
+
+        return redirect('/book')  # always redirect after POST
+
+    else:  # is GET request, return the add book HTML
+        return render(request, 'add_book.html')
+
+
+def book_view(request):
+    check_books_list(request)
+
+    print(request.session['books'])
+
+    return render(request, 'book.html', {
+        'books': request.session['books']
+    })
+
+
+def check_books_list(request):
+    if 'books' not in request.session:
+        request.session['books'] = []
+
+
+def index_view(request):
+    day_of_week = datetime.today().weekday()  # returns a number 0 - 7; 0 = Monday
 
     context = {
         'todays_hours': HOURS_OF_OPERATION[day_of_week],
@@ -22,3 +65,15 @@ def index_view(request):
     }
 
     return render(request, 'index.html', context)
+
+
+def view_book(request, book_id):
+    check_books_list(request)
+
+    # validation
+    if book_id > len(request.session['books']) - 1:
+        return redirect('/book')
+
+    return render(request, 'view_book.html', {
+        'book': request.session['books'][book_id]
+    })
